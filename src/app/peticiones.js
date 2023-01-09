28383e425e4df90c79dcd181a9c606d3515cfbeb
Adrián <39964431+adriib38@users.noreply.github.com>
2023-01-09 12:34:58 +0100

    Add files via upload

--- src/app/peticiones.js
+++ src/app/peticiones.js
@@ -1,2 +1,37 @@
-console.log('peticiones.js');
-alert('hola');
+const getUsers = async () => {
+    //Hacer un request a la API y obtener un objeto que contenga los datos
+    const res = await fetch(`http://localhost:3000/api/publications`, {
+        "method": "GET",
+        "headers": {
+            "Content-Type": "Application/json",
+        }
+    })
+    const data = await res.json();
+
+    printMuro(data);
+}
+getUsers();
+
+function printMuro(publications){
+    let muro = document.getElementById('muro');
+    console.log(publications);
+
+    for(publication of publications){
+        let date = moment(publication.date).fromNow(); 
+        muro.innerHTML += `
+        
+        <div class="card">
+            <div class="card-header">
+                ${publication.author}
+            </div>
+            <div class="card-body">
+                <blockquote class="blockquote mb-0">
+                <p>${publication.content}.</p>
+                <footer class="blockquote-footer"><cite title="Sourcer Title"></cite>${date}</footer>
+                </blockquote>
+            </div>
+        </div>
+        <br>
+        `;
+    };   
+}
--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ const express = require('express');
 const mongoose = require("mongoose");
 require("dotenv").config();
 const userRoutes = require("./routes/user");
+
 const publicationRoutes = require("./routes/publications");
 const app = express();
+const morgan = require('morgan');
 const port = process.env.PORT || 3000;
 
 //middleware
@@ -11,10 +13,21 @@ app.use(express.json())
 app.use('/api', userRoutes);
 app.use('/api', publicationRoutes);
 
-
 //routes
 app.get("/", (req, res) => {
     res.sendFile('app/index.html', {root: __dirname })
+})
+
+app.get("/", (req, res) => {
+    res.sendFile('app/index.html', {root: __dirname })
+})
+
+app.get("/style.css", (req, res) => {
+    res.sendFile('app/style.css', {root: __dirname })
+})
+
+app.get("/peticiones.js", (req, res) => {
+    res.sendFile('app/peticiones.js', {root: __dirname })
 })
 
 app.get("/publicar", (req, res) => {
@@ -26,5 +39,7 @@ mongoose
 .connect(process.env.MONGODB_URI)
 .then(() => console.log('Connected to MongoDB Atlas'))
 .catch((error) => console.error(error));
+
+app.use(morgan('dev'));
 
 app.listen(port, () => console.log('Server listening on port', port))
